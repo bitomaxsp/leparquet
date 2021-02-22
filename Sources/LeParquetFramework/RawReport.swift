f6eeee58a5714928cd08e520b62b166cab86ed59
bitomaxsp <bitomaxsp@gmail.com>
2021-02-22 08:26:29 +0100

    Reporting

--- Sources/LeParquetFramework/RawReport.swift
+++ Sources/LeParquetFramework/RawReport.swift
@@ -14,11 +14,17 @@ class RawReport {
     typealias StashOfRights = [RightCut]
 
     init(_ input: LayoutInput) {
+        self.input = input
         self.first_row_height = Double(input.material.board.size.height)
-        self.boardHeight = input.material.board.size.height
     }
 
-    let boardHeight: Double
+    let input: LayoutInput
+    var boardHeight: Double { return self.input.material.board.size.height }
+
+    var boardArea: Double { return self.input.material.board.area }
+
+    var roomSize: Config.Size { return self.input.effectiveRoomSize }
+
     var first_row_height: Double
     var last_row_height = 0.0
 
@@ -35,6 +41,7 @@ class RawReport {
     var unused_height_on_first_row: Double = 0.0
     var unused_height_on_last_row: Double = 0.0
     var total_rows = 0
+    var boardNumWithMargin = 0
 
     // MARK: Implementation
 
@@ -65,6 +72,42 @@ class RawReport {
     func printAll() {
         print("Unusable normalized rests: \(self.unusable_rest.map { $0.width.round(4) }) [norm to board length]")
         self.printRows()
+
+        print("Unusable rests: \(self.unusable_rest.map { ($0.width * self.input.material.board.size.width).round(4) }) mm")
+
+        let rest_width_sum = self.unusable_rest.reduce(0.0) { (next, b) in
+            return next + b.width
+        }
+
+        let unused_area = rest_width_sum * self.boardArea
+        print("Unusable side trash area: \(unused_area.round(4)) m^2")
+
+        let side_cut_trash_area_m2 = (Double(self.total_rows) * self.boardHeight - self.roomSize.height) * self.roomSize.width * 1e-6
+        let total_trash = unused_area + side_cut_trash_area_m2
+
+        print("Unusable top/bottom trash area: \(side_cut_trash_area_m2) m^2")
+        print("Total trash area: \(total_trash.round(4)) m^2")
+        print("Used boards: \(self.used_boards)")
+
+        let total_board_area = Double(self.used_boards) * self.boardArea
+
+        // TODO: self.input.material.pack.area
+//        let packs_required = ceil(total_board_area / self.input.material.pack.area)
+//        print("Unsed boards left: {packs_required * int(round(self.pack_area_m2/self.one_board_area_m2, 0)) - self.used_boards}")
+//        print("Packs required: {packs_required}")
+        print("Total buy area as [boards * board area]: \(total_board_area) m^2")
+        print("Total buy area - total trash area: \((total_board_area - total_trash).round(4)) m^2")
+
+        print("\n----------- THEROY DATA: -----------")
+
+        print("Calculated area: \(self.input.calc_covered_area.round(4)) m^2")
+        print("Calculated area + margin: \(self.input.calc_covered_area_with_margin.round(4)) m^2")
+        self.boardNumWithMargin = Int(ceil(self.input.calc_covered_area_with_margin / self.boardArea))
+        print("Calculated boards (using margin), float: \((self.input.calc_covered_area_with_margin / self.boardArea).round(4))")
+        print("Calculated boards (using margin), int: \(self.boardNumWithMargin)")
+
+        print("Total trash calc: \((total_board_area - self.input.calc_covered_area).round(4)) m^2")
+//        print(self.summary)
     }
 
     func printRows() {
@@ -78,33 +121,3 @@ class RawReport {
 }
 
 // let mul = 1.0 * board_width
-
-//        print("Unusable rests: {[round(x*self.board_width, 3) for x in self.unusable_rest]} mm")
-
-//        let unused_area = sum(self.unusable_rest) * self.input.material.board.area
-//        print("Unusable side trash area: {round(unused_area, 4)} m^2")
-
-//        let side_cut_trash_area_m2 = (self.total_rows * self.board_height - self.roomW) * self.roomL * 1e-6
-//        let total_trash = unused_area + side_cut_trash_area_m2
-
-//        print("Unusable top/bottom trash area: {side_cut_trash_area_m2} m^2")
-//        print("Total trash area: {round(total_trash, 4)} m^2")
-//        print("Used boards: {self.used_boards}")
-
-//        let total_board_area = self.state.used_boards * self.one_board_area_m2  // - side_cut_trash_area  // - 2 * self.roomL*
-//        let packs_required = math.ceil(total_board_area / self.pack_area_m2)
-//        print("Unsed boards left: {packs_required * int(round(self.pack_area_m2/self.one_board_area_m2, 0)) - self.used_boards}")
-//        print("Packs required: {packs_required}")
-//        print("Total buy area as [boards * board area]: {total_board_area} m^2")
-//        print("Total buy area - total trash area: {round(total_board_area - total_trash, 4)} m^2")
-
-//        print('\n----------- THEROY DATA: -----------")
-
-//        print("Calculated area: {self.calc_covered_area} m^2")
-//        print("Calculated area + margin: {self.calc_covered_area_with_margin} m^2")
-//        self.int_board_num = math.ceil(self.calc_covered_area_with_margin / self.one_board_area_m2)
-//        print("Calculated boards (using margin), float: {self.calc_covered_area_with_margin/self.one_board_area_m2}")
-//        print("Calculated boards (using margin), int: {self.int_board_num}")
-
-//        print("Total trash calc: {total_board_area - self.calc_covered_area} m^2")
-//        print(self.summary)
